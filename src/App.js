629fa0681dfc3d57e09068b76659f6dd95ee7316
Hany Raza <hanyraza@Hanys-MacBook-Pro.local>
2021-04-24 17:34:44 -0400

    Assignment 7

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import CourseManager from "./components/course-manager";
+import CourseEditor from "./components/course-editor/course-editor";
+import {BrowserRouter, Route} from "react-router-dom";
+import Home from "./components/home";
+import QuizzesList from "./components/quizzes/quizzes-list";
+import Quiz from "./components/quizzes/quiz";
+
+function App() {
+  return (
+      <BrowserRouter>
+        <div className="container-fluid">
+          <Route path="/" exact={true}  component={Home}/>
+            <Route path="/courses" component={CourseManager}/>
+            <Route path="/courses/:courseId/quizzes" exact={true}>
+                <QuizzesList/>
+            </Route>
+            <Route path="/courses/:courseId/quizzes/:quizId" exact={true}>
+                <Quiz/>
+            </Route>
+          <Route path="/course" component={CourseManager}/>
+          {/*<Route path="/editor" component={CourseEditor}/>*/}
+          {/*<Route path="/editor" exact={true} render={(props) => <CourseEditor {...props}/>}/>*/}
+            <Route path={[
+                           "/courses/:layout/edit/:courseId",
+                           "/courses/:layout/edit/:courseId/modules/:moduleId",
+                           "/courses/:layout/edit/:courseId/modules/:moduleId/lessons/:lessonId",
+                           "/courses/:layout/edit/:courseId/modules/:moduleId/lessons/:lessonId/topics/:topicId",
+                           "/courses/:layout/edit/:courseId/modules/:moduleId/lessons/:lessonId/topics/:topicId/widgets/:widgetId"
+                       ]}
+                   exact={true}
+                   render={(props) => <CourseEditor {...props}/>}/>
+        </div>
+      </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/services/widget-service.js
@@ -0,0 +1,54 @@
+// const TOPICS_URL="https://webdev-webapp-hany.herokuapp.com/api/topics";
+// const WIDGETS_URL="https://webdev-webapp-hany.herokuapp.com/api/widgets";
+
+const TOPICS_URL="http://localhost:8080/api/topics";
+const WIDGETS_URL="http://localhost:8080/api/widgets";
+
+
+export const findWidgetsForTopic = (topicId) =>
+    fetch(`${TOPICS_URL}/${topicId}/widgets`)
+        .then(response => response.json())
+
+
+export const createWidgetForTopic = (topicId,widget) =>
+    // TODO: move all server communication to widgets-service
+    fetch(`${TOPICS_URL}/${topicId}/widgets`, {
+        method: 'POST',
+        body: JSON.stringify(widget),
+        headers: {
+            "content-type": 'application/json'
+        }
+    })
+        .then(response => response.json())
+        // .then(widget => setWidgets((widgets) => [...widgets, widget]))
+
+
+export const deleteWidget = (widget_id) =>
+    // TODO: move all server communication to widgets-service.js
+    fetch(`${WIDGETS_URL}/${widget_id}`, {
+        method: "DELETE"
+    }
+    )
+        .then(response => response.json())
+    //     .then((status) => {
+    //     // setWidgets((widgets) => widgets.filter(w => w.id !== id))
+    // })
+
+export const updateWidget = (widget) =>
+    // TODO: move all server communication to widgets-service.js
+    fetch(`${WIDGETS_URL}/${widget.id}`, {
+        method: "PUT",
+        body: JSON.stringify(widget),
+        headers: {
+            "content-type": 'application/json'
+        }
+    })
+        .then(response => response.json())
+    //     .then((status) => {
+    //     setWidget({})
+    //     setWidgets((widgets) => widgets.map(w => w.id === id ? widget : w))
+    // })
+
+    export default {
+    createWidgetForTopic, findWidgetsForTopic, deleteWidget, updateWidget
+}
